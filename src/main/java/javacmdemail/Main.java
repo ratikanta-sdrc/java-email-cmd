bb730fdfc612ec6d5265bdec6a2e287f4a74fdc9
ratikanta-sdrc <ratikanta@sdrc.co.in>
2018-10-25 12:01:52 +0530

    Line of code optimized

--- src/main/java/javacmdemail/Main.java
+++ src/main/java/javacmdemail/Main.java
@@ -18,26 +18,17 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		
-		String fromUsername = null;
-		String password = null;
-		String toEmailId = null;
-		String subject = null;
-		String body = null;
 		try{
 			JSONParser parser = new JSONParser();
 			Object obj = parser.parse(new FileReader(args[0]));
-
             JSONObject jsonObject = (JSONObject) obj;
-			
-			fromUsername = (String) jsonObject.get("fromUsername");
-			password = (String) jsonObject.get("password");
-			toEmailId = (String) jsonObject.get("toEmailId");
-			subject = (String) jsonObject.get("subject");
-			body = (String) jsonObject.get("body");
 		
 			Main main = new Main();
-			main.sendMail(fromUsername, password, toEmailId, subject, body);
+			main.sendMail((String) jsonObject.get("fromUsername")
+			, (String) jsonObject.get("password")
+			, (String) jsonObject.get("toEmailId")
+			, (String) jsonObject.get("subject")
+			, (String) jsonObject.get("body"));
 			
 		}catch(Exception e){
 			System.out.println("Invalid input " + e.getMessage());
@@ -48,36 +39,36 @@ public class Main {
 	void sendMail(String fromUsername, String fromUserPassword, String toEmailId, String subject, String body){
 		
 		try{
-		final String username = fromUsername;
-		final String password = fromUserPassword;
+			final String username = fromUsername;
+			final String password = fromUserPassword;
 
-		Properties props = new Properties();
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.port", "587");
+			Properties props = new Properties();
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.starttls.enable", "true");
+			props.put("mail.smtp.host", "smtp.gmail.com");
+			props.put("mail.smtp.port", "587");
 
-		Session session = Session.getInstance(props,
-		  new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(username, password);
+			Session session = Session.getInstance(props,
+			new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
+			});
+
+			try {
+
+				Message message = new MimeMessage(session);
+				message.setFrom(new InternetAddress(username));
+				message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(toEmailId));
+				message.setSubject(subject);
+				message.setText(body);
+				Transport.send(message);
+				System.out.println("Email sent");
+
+			} catch (MessagingException e) {
+				throw new RuntimeException(e);
 			}
-		  });
-
-		try {
-
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(username));
-			message.setRecipients(Message.RecipientType.TO,
-				InternetAddress.parse(toEmailId));
-			message.setSubject(subject);
-			message.setText(body);
-			Transport.send(message);
-			System.out.println("Email sent");
-
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
-		}
 		}catch(Exception e){
 			System.out.println("Could not send email " + e.getMessage());
 		}
